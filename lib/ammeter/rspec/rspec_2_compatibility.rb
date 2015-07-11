7b12c7104d8a8d03d686774dc87a33926ba824b4
Ashley Engelund (aenw / weedySeaDragon) <ashley@ashleycaroline.com>
2015-07-11 02:03:13 -0700

    define Rspec::Core::MemoizedHelpers if it's not yet defined (for compatibility with RSpec 2)

--- lib/ammeter/rspec/rspec_2_compatibility.rb
+++ lib/ammeter/rspec/rspec_2_compatibility.rb
@@ -2,7 +2,7 @@ require 'rspec/expectations'
 if RSpec::Expectations::Version::STRING < '3'
 
   { :match_when_negated => :match_for_should_not,
-    :failure_message    => :failure_message_for_should,
+    :failure_message => :failure_message_for_should,
     :failure_message_when_negated => :failure_message_for_should_not
   }.each do |rspec3_method, rspec2_method|
     RSpec::Matchers::DSL::Matcher.send :alias_method, rspec3_method, rspec2_method
@@ -10,7 +10,15 @@ if RSpec::Expectations::Version::STRING < '3'
 
   module RSpec2CoreMemoizedHelpers
     def is_expected
-       expect(subject)
+      expect(subject)
+    end
+  end
+  if !defined? RSpec::Core::MemoizedHelpers
+    module RSpec
+      module Core
+        module MemoizedHelpers
+        end
+      end
     end
   end
   RSpec::Core::MemoizedHelpers.extend RSpec2CoreMemoizedHelpers
